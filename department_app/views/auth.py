ee89a9d98c835220751842848415f0b2082d87d5
aer0z <gerasimovichba@gmail.com>
2021-12-10 23:24:25 +0200

    sign up update

--- department_app/views/auth.py
+++ department_app/views/auth.py
@@ -1,6 +1,5 @@
-
 from . import page
-from flask import render_template
+from flask import render_template, request, flash
 
 
 @page.route('/')
@@ -8,7 +7,7 @@ def home():
     return render_template("home.html")
 
 
-@page.route('/login')
+@page.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
@@ -18,6 +17,22 @@ def logout():
     return '<p>Logout</p>'
 
 
-@page.route('/sign-up')
+@page.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            # add user to database
+            flash('Account created!', category='success')
     return render_template('sign_up.html')
